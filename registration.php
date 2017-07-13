fbc83bfcbbec217cbcd626b3301886053cff5662
Vasiliy Seleznev <vseleznev@magento.com>
2017-07-13 11:43:03 -0500

    Updating theme path in registration.php

--- registration.php
+++ registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Magento/vasiliy',
+    'frontend/vasiliyseleznev/vasiliy',
     __DIR__
 );
